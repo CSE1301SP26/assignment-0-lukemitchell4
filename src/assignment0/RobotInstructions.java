a43d7ec09254161ef34d8b4aca58dbf77ff53fee
lukemitchell4 <m.luke@wustl.edu>
2026-01-14 18:33:04 -0600

    Luke Mitchell Assignment 0 Complete

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,34 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
 		robot.forward(80);
+		robot.turnRight(90);
+		robot.turnRight(90);
+		robot.forward(20);
+		robot.turnLeft(90);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(100);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(45);
+		robot.forward(30);
+		robot.turnLeft(90);
+		robot.forward(20);
+		robot.turnRight(180);
+		robot.forward(80);
+		robot.turnLeft(90);
+		robot.forward(62);
 	}
 
 }
